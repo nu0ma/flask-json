b0b50fa60513b779bc527586c7130e62c9963126
Ryota Ishikawa <c126001a@comwork.c.oka-pu.ac.jp>
2017-08-17 20:14:17 +0900

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -40,21 +40,6 @@ def post_request():
     save_name = keys_array[0] + ".jpg"
     #コンバート
     convert_b64_to_file(bytes(values_array[0],"utf-8"),save_name)
-    #ここにkey毎の処理を書いていく
-    ###このやり方だと，image.jpgがいくつも生成されることになってしまう
-    # for i in range(len(keys_array)):
-    #     #まずはkeyが画像の場合
-    #     if keys_array[i] == "image":
-    #         save_name = keys_array[i] + ".jpg"
-    #         convert_b64_to_file(bytes(values_array[i],"utf-8"),save_name)
-
-    #utf-8に変換したいけどdict型なのでできない
-    #value = bytes(value,"utf-8")
-    #ファイル名はとりあえずkeyとしておく
-    #filename = key + ".jpg"
-    #デコード
-    #convert_b64_to_file(value,filename)
-    #convert_b64_to_file(value,"./test.jpg")
 
     return jsonify(res='success', **data)
 
@@ -62,4 +47,4 @@ def post_request():
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0")
-    # curl http://127.0.0.1:5000/post_request -X POST -H "Content-Type: application/json" -}''{"key": "value"}
+    # curl http://0.0.0.0/post_request -X POST -H "Content-Type: application/json" -}''{"key": "value"}
